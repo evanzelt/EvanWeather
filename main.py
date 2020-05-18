dd6810041c3049d3c095a9f805f05e74c2812a8f
ezelt <bplaxbro@gmail.com>
2020-05-18 00:21:59 -0400

    Version 1.0

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ def main():
     while(1):
         if(time() - last_time > delay):
             updateWeather(main_city, my_account)
+            last_time = time()
 
 
 def updateWeather(city, account):
     weather_data = getWeatherData(city)
     cities = closestFarthestCities(city)
-    today_date = date.today().strftime("%m/%d/%Y")
-    tweet = "{}\n{} has {} with a temperature of {}°F today.\nComparable - {} with {} and a temperature of {}°F.\nDissimilar - {} with {} and a temperature of {}°F".format(today_date, city, weather_data["weather"][0]["description"], weather_data["main"]["temp"], cities[0][0], cities[0][2], cities[0][1], cities[1][0], cities[1][2], cities[1][1])
+    tweet = "{} has {} with a temperature of {}°F today.\nComparable - {} with {} and a temperature of {}°F.\nDissimilar - {} with {} and a temperature of {}°F".format(city, weather_data["weather"][0]["description"], weather_data["main"]["temp"], cities[0][0], cities[0][2], cities[0][1], cities[1][0], cities[1][2], cities[1][1])
     print(tweet)
     account.sendTweet(tweet)
 
